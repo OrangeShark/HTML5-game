72fd14ae1181c79451841ac87adf611b73d2f501
Erik <erik.edrosa@gmail.com>
2013-03-12 00:34:11 -0400

    Added TiledMap loading and parsing Added Input.js file for Input Engine

--- scripts/MapTiles.js
+++ scripts/MapTiles.js
@@ -1,4 +1,4 @@
-function TiledMap() {
+function TiledMap(mapUrl) {
 
     this.currMapData = null;
     this.tilesets = [];
@@ -14,4 +14,87 @@ function TiledMap() {
     };
     this.imgLoadCount = 0;
     this.fullyLoaded = false;
+
+    var self = this;
+
+    var parseMap = function(mapURL) {
+        self.currMapData = JSON.parse(mapJSON);
+
+        var map = self.currMapData;
+
+        self.numXTiles = map.width;
+        self.numYTiles = map.height;
+
+        self.tileSize.x = map.tilewidth;
+        self.tileSize.y = map.tileheight;
+
+        self.pixelSize.x = self.numXTiles * self.tileSize.x;
+        self.pixelSize.y = self.numYTiles * self.tileSize.y;
+
+        for(var i = 0; i < map.tilesets.length; i++) {
+
+            var tileset = map.tilesets[i];
+
+            var img = new Image();
+            img.onload = function() {
+                self.imgLoadCount++;
+                if( self.imgLoadCount === map.tilesets.length) {
+                    self.fullyLoaded = true;
+                }
+            };
+
+            img.src = tileset.image;
+
+            var tile = new Tile( 
+                    tileset.firstgid,
+                    img,
+                    tileset.imageheight,
+                    tileset.imagewidth,
+                    tileset.name,
+                    Math.floor(tileset.imagewidth / self.tileSize.x),
+                    Math.floor(tileset.imageheight / self.tileSize.y));
+
+            self.tilesets[i] = tile;
+        }
+    };
+
+    xhrGet( mapUrl, function( data ) {
+        parseMap(data.responseText);
+    }
+
+}
+
+TiledMap.prototype.getTilePacket = function(tileIndex) {
+    var pkt = {
+        img: null,
+        px: 0,
+        py: 0
+    };
+
+    var tile = 0;
+    for( tile = this.tilesets.length - 1; tile >= 0; tile --) {
+        if(this.tilesets[tile].firstgid <= tileIndex) break;
+    }
+
+    pkt.img = this.tilesets[tile].image;
+
+    var localIdx = tileIndex - this.tilesets[tile].firstgid;
+
+    var lTileX = localIdx % this.tilesets[tile].numXTiles;
+    var lTileY = Math.floor( localIdx / this.tilesets[tile].numXTiles);
+
+    pkt.px = lTileX * this.tileSize.x;
+    pkt.py = lTileY * this.tileSize.y;
+
+    return pkt;
+}
+
+function Tile( firstgid, image, imageheight, imagewidth, name, numXTiles, numYTiles) {
+    this.firstgid = firstgid;
+    this.image = image;
+    this.imageheight = imageheight;
+    this.imagewidth = imagewidth;
+    this.name = name;
+    this.numXTiles = numXTiles;
+    this.numYTiles = numYTiles;
 }
